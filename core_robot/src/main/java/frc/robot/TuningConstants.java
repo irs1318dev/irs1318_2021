2276112c4b5031aa501c80e377eaef5904eb7598
vanshikaaa <vanshikabalaji@outlook.com>
2021-03-15 21:47:30 -0700

    and powercellmechanism

--- core_robot/src/main/java/frc/robot/TuningConstants.java
+++ core_robot/src/main/java/frc/robot/TuningConstants.java
@@ -43,6 +43,7 @@ public class TuningConstants
         mechanismList.add(injector.getInstance(PowerManager.class));
         mechanismList.add(injector.getInstance(DriveTrainMechanism.class));
         mechanismList.add(injector.getInstance(CompressorMechanism.class));
+        mechanismList.add(injector.getInstance(PowerCellMechanism.class));
         // mechanismList.add(injector.getInstance(OffboardVisionManager.class));
         // mechanismList.add(injector.getInstance(IndicatorLightManager.class));
         return mechanismList;
