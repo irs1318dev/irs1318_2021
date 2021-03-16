b1c5e7d8af9ad36d82095aa833a5731437ef46d9
vanshikaaa <vanshikabalaji@outlook.com>
2021-03-15 21:43:00 -0700

    enabled compressor mechanism

--- core_robot/src/main/java/frc/robot/TuningConstants.java
+++ core_robot/src/main/java/frc/robot/TuningConstants.java
@@ -42,7 +42,7 @@ public class TuningConstants
         mechanismList.add(injector.getInstance(NavxManager.class)); // NavxManager should come before DriveTrainMechanism
         mechanismList.add(injector.getInstance(PowerManager.class));
         mechanismList.add(injector.getInstance(DriveTrainMechanism.class));
-        // mechanismList.add(injector.getInstance(CompressorMechanism.class));
+        mechanismList.add(injector.getInstance(CompressorMechanism.class));
         // mechanismList.add(injector.getInstance(OffboardVisionManager.class));
         // mechanismList.add(injector.getInstance(IndicatorLightManager.class));
         return mechanismList;
