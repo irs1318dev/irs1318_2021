8c49c3533f6a5e2192a6a21a3431aab9d11219fd
IRS1318 DriverStation20A <issaquahroboticssociety@outlook.com>
2021-09-11 17:01:19 -0700

    Fixes of things for testing 2021 robot

--- core_robot/src/main/java/frc/robot/HardwareConstants.java
+++ core_robot/src/main/java/frc/robot/HardwareConstants.java
@@ -14,7 +14,7 @@ public class HardwareConstants
     // Vision Alignment 
     public static final double CAMERA_PITCH = 22.5; // in degrees
     public static final double CAMERA_X_OFFSET = 0.0; // in inches
-    public static final double CAMERA_Z_OFFSET = 21.0; // in inches
+    public static final double CAMERA_Z_OFFSET = 18.0; // in inches
     public static final double VISIONTARGET_Z_OFFSET = 90.25; // in inches
     public static final double CAMERA_TO_TARGET_Z_OFFSET = HardwareConstants.VISIONTARGET_Z_OFFSET - HardwareConstants.CAMERA_Z_OFFSET;
     public static final double CAMERA_YAW = 0.0; // in degrees
